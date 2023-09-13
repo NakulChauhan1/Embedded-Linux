59bd6fd8789bc777ab91a4e50fb19d1a79e19895
nakulchauhan-cpi <112661494+nakulchauhan-cpi@users.noreply.github.com>
2023-09-13 13:08:24 +0530

    Things done: (#2)
    - Dynamically generate major number, minor number
    - device file creation (check: ls /dev/hello* -la)
    
    remove non related code

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -1,15 +1,72 @@
-
 #include<linux/module.h>
 #include<linux/kernel.h>
+#include<linux/fs.h>
+#include <linux/kdev_t.h>
+#include <linux/types.h>
+#include <linux/device.h>
+
+static dev_t dev = 0;
+static unsigned int count = 3;
+static struct class *cls = NULL;
+static struct device *d = NULL;
 
 static int __init hello_world_init(void)
 {
-    printk("Hello World \n");
+    unsigned int baseminor = 0;
+
+    //register a range of char device numbers (Generate major number, minor number)
+    if(alloc_chrdev_region(&dev, baseminor, count, "helloworld") < 0)
+    {
+        printk("Hello World Char device number registration failed \n");
+        return -1;
+    }
+
+    printk("Hello World Kernel Module Inserted succesffully \n");
+    printk("Major = %d, Minor = %d \n",MAJOR(dev), MINOR(dev));
+
+    /*
+    Check the major number using cat /proc/devices, note: device file is not created yet, so nothing will be under /dev
+
+    
+    Manual way of creating device file: mknod -m <permissions> <name> <device type> <major> <minor>
+    Automatic creation of device files can be handled with udev. Udev is the device manager for the Linux kernel that creates/removes device nodes in the /dev directory dynamically.
+    */
+   
+   /*Creating struct class*/
+   cls = class_create(THIS_MODULE, "Others");
+        
+    if(IS_ERR(cls))
+    {
+        printk("Cannot create the struct class for device\n");
+        goto r_class;
+    }
+
+    /*Creating device (device file)*/    
+    d = device_create(cls, NULL, dev, NULL, "helloworld");
+
+    if(IS_ERR(d))
+    {
+        printk("Cannot create the Device\n");
+        goto r_device;
+    }
+
     return 0;
+
+    r_device:
+        class_destroy(cls);
+    
+    r_class:
+        unregister_chrdev_region(dev,1);
+        return -1;
 }
 
 static void __exit hello_world_exit(void)
 {
+    device_destroy(cls, dev);
+    class_destroy(cls);
+    unregister_chrdev_region(dev, count);
+
+    printk("Device Destroyed,  Class Destroyed, Dev unregistered \n");
     printk("By By World \n");
 }
 
