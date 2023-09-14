10bdb7cd78f6cbb8c7850718ac1a1b3bb57bbc25
Nakul Chauhan <48352040+NakulChauhan1@users.noreply.github.com>
2023-09-14 21:53:13 +0530

    Nakul/hello world syscall to driver (#3)
    * map Application layer syscalls to drivers specific syscalls (file_operation struct is important for this, and cdev_init()
     to initialise this structure to cdev struct and cdev_add() for adding it to kernel)
    
    ---------
    
    Co-authored-by: Nakul Chauhan <nakul.chauhan@chargepoint.com>

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -4,15 +4,59 @@
 #include <linux/kdev_t.h>
 #include <linux/types.h>
 #include <linux/device.h>
+#include <linux/cdev.h>
 
 static dev_t dev = 0;
 static unsigned int count = 3;
 static struct class *cls = NULL;
 static struct device *d = NULL;
+static struct cdev char_device;
+
+static int driver_open (struct inode *in, struct file *f)
+{
+    printk("Hello World: Device file opened \n");
+    return 0;
+}
+
+static ssize_t driver_read (struct file *f, char __user *data, size_t size, loff_t *off)
+{
+    printk("Hello World: Device file read \n");
+    return 0;
+}
+
+static ssize_t driver_write (struct file *, const char __user *data, size_t size, loff_t *off)
+{
+    printk("Hello World: Device file written with %s(%ld)\n", data, size);
+    return size;
+}
+
+static int driver_release (struct inode *, struct file *)
+{
+    printk("Hello World: Device file closed \n");
+    return 0;
+}
+
+/*
+static int driver_ioctl (struct file *, unsigned int, unsigned long)
+{
+    printk("Hello World: Device file sent small data (ioctl) \n");
+}
+*/
+
+static const struct file_operations fops = 
+{                              \
+    .owner      = THIS_MODULE,                                         \
+    .open       = &driver_open,                                        \
+    .read       = &driver_read,                                        \
+    .write      = &driver_write,                                       \
+    .release    = &driver_release,                                       \
+};
+
 
 static int __init hello_world_init(void)
 {
     unsigned int baseminor = 0;
+    int r;
 
     //register a range of char device numbers (Generate major number, minor number)
     if(alloc_chrdev_region(&dev, baseminor, count, "helloworld") < 0)
@@ -50,14 +94,30 @@ static int __init hello_world_init(void)
         goto r_device;
     }
 
+    // Initialize cdev structure
+    cdev_init(&char_device, &fops);
+
+    // add (tell about) fop initialized cdev structure to kernel 
+    r = cdev_add(&char_device, dev, count);
+
+    if(r<0)
+    {
+        printk("cdev addition to the kernal failed \n");
+        goto r_cdev;
+    }
+
     return 0;
+
+    r_cdev:
+        device_destroy(cls, dev);
 
     r_device:
         class_destroy(cls);
     
     r_class:
         unregister_chrdev_region(dev,1);
-        return -1;
+
+    return -1;
 }
 
 static void __exit hello_world_exit(void)
@@ -65,8 +125,9 @@ static void __exit hello_world_exit(void)
     device_destroy(cls, dev);
     class_destroy(cls);
     unregister_chrdev_region(dev, count);
+    cdev_del(&char_device);
 
-    printk("Device Destroyed,  Class Destroyed, Dev unregistered \n");
+    printk("Device Destroyed,  Class Destroyed, Dev unregistered, cdev removed from system \n");
     printk("By By World \n");
 }
 
